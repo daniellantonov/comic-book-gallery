54e11839cf62fae95a1975c95da88a4144e9c428
daniellantonov <daniellantonov@gmail.com>
2019-03-15 16:59:35 +0200

    start

---
+++ comic-book-gallery/Controllers/ComicBookController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace comic_book_gallery.Controllers
+{
+    public class ComicBookController : Controller
+    {
+        public ActionResult Detail()
+        {
+            return View(); 
+        }
+    }
+}
